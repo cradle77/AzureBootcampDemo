690e0bf718d823cf5ed569f1efd7c3ab70c461cd
marcodes <marcodesanctis2@gmail.com>
2020-04-19 14:47:50 +0100

    class libraries

---
+++ WeatherForecasts.Service/IWeatherForecastService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using WeatherForecasts.Shared;
+
+namespace WeatherForecasts.Service
+{
+    public interface IWeatherForecastService
+    {
+        IEnumerable<WeatherForecast> GetForecast();
+    }
+}
---
+++ WeatherForecasts.Service/WeatherForecastService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WeatherForecasts.Shared;
+
+namespace WeatherForecasts.Service
+{
+    public class WeatherForecastService : IWeatherForecastService
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastService> _logger;
+
+        public WeatherForecastService(ILogger<WeatherForecastService> logger)
+        {
+            _logger = logger;
+        }
+
+        public IEnumerable<WeatherForecast> GetForecast()
+        {
+            _logger.LogInformation("GetForecast was called");
+
+            var rng = new Random();
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
